738da8d0cb372fb576b81e7f04cffe741ee236c2
cyaris <charlieyaris@gmail.com>
2018-11-02 15:38:19 -0500

    Edited dashboard scripts.

--- _includes/tableau/post_dashboard_2.js
+++ _includes/tableau/post_dashboard_2.js
@@ -2,78 +2,73 @@
    * Instantiate our vizualization.
    */
 
-  $(function() {
-    var url  = 'https://public.tableau.com/views/CourtSentencinginCookCountyIL/Question?/Splash'
-    var size = findDashboardSize();
-    var vizOptions = {
-      showTabs           : true,
-      hideToolbar        : true,
-      width              : size.width + 'px',
-      height             : size.height + 'px',
-      onFirstInteractive : function (e) {
-        $('#viz').width(size.width);
-        $('#viz').height(size.height);
-        $('#viz iframe').attr('scrolling', 'no');
-        // $('#viz iframe').css('overflowx', 'visible');
-        // $('#viz iframe').css('overflowy', 'visible');
-        $('#viz iframe').width(size.width);
-        $('#viz iframe').height(size.height + 20);
-      }
-    };
-    var viz = new tableauSoftware.Viz(document.getElementById('viz'), url, vizOptions);
-    viz.addEventListener(tableau.TableauEventName.TAB_SWITCH, function resize_again() {
-        console.log('again')
-        var size = findDashboardSize();
-        $('#viz').width(size.width);
-        $('#viz').height(size.height);
-        $('#viz iframe').attr('scrolling', 'no');
-        // $('#viz iframe').css('overflowx', 'visible');
-        // $('#viz iframe').css('overflowy', 'visible');
-        $('#viz iframe').width(size.width);
-        $('#viz iframe').height(size.height + 20);
-        });
-  });
+   $(function() {
+     var url  = 'https://public.tableau.com/views/CourtSentencinginCookCountyIL/Question?/Splash'
+     var size = findDashboardSize();
+     var vizOptions = {
+       showTabs           : true,
+       // hideTabs           : true,
+       hideToolbar        : true,
+       width              : size.width + 'px',
+       height             : size.height + 'px',
+       onFirstInteractive : function (e) {
+         $('#viz iframe').attr('scrolling', 'no');
+         // $('#viz iframe').css('overflowx', 'visible');
+         // $('#viz iframe').css('overflowy', 'visible');
+         $('#viz iframe').width(size.width);
+         $('#viz iframe').height(size.height + 20);
+       }
+     };
+     var viz = new tableauSoftware.Viz(document.getElementById('viz'), url, vizOptions);
+     viz.addEventListener(tableau.TableauEventName.TAB_SWITCH, function resize_again() {
+         console.log('again')
+         var size = findDashboardSize();
+         $('#viz iframe').attr('scrolling', 'no');
+         // $('#viz iframe').css('overflowx', 'visible');
+         // $('#viz iframe').css('overflowy', 'visible');
+         $('#viz iframe').width(size.width);
+         $('#viz iframe').height(size.height + 20);
+         });
+   });
 
-  /**
-   * Finds the correct size of the dashboard in this screen.
-   */
-  function findDashboardSize() {
-    var minHeight = 800;
-    var maxHeight = 900;
-    var minWidth  = 1000;
+   /**
+    * Finds the correct size of the dashboard in this screen.
+    */
+   function findDashboardSize() {
+     var minHeight = 600;
+     var maxHeight = 885;
+     var minWidth  = 1000;
 
-    // The minus 100px leaves us room for the header/footer. Adjust as needed.
-    var height = $('body').height() - 100;
+     // The minus 100px leaves us room for the header/footer. Adjust as needed.
+     var height = $('body').height() - 100;
 
-    if (height < minHeight) {
-      height = minHeight;
-    }
-    if (height > maxHeight) {
-      height = maxHeight;
-    }
+     if (height < minHeight) {
+       height = minHeight;
+     }
+     if (height > maxHeight) {
+       height = maxHeight;
+     }
 
-    var width = Math.round(height/(minHeight/minWidth));
-    return {
-      'width'  : width,
-      'height' : height
-    }
-  }
+     var width = Math.round(height/(minHeight/minWidth));
+     return {
+       'width'  : width,
+       'height' : height
+     }
+   }
 
-  /**
-   * Resizes our viz.
-   */
-  function resize() {
-    var size = findDashboardSize();
-    $('#viz').width(size.width);
-    $('#viz').height(size.height);
-    $('#viz iframe').attr('scrolling', 'no');
-  	// $('#viz iframe').css('overflowx', 'visible');
-  	// $('#viz iframe').css('overflowy', 'visible');
-    $('#viz iframe').width(size.width);
-    $('#viz iframe').height(size.height + 20);
-  }
+   /**
+    * Resizes our viz.
+    */
+   function resize() {
+     var size = findDashboardSize();
+     $('#viz iframe').attr('scrolling', 'no');
+     // $('#viz iframe').css('overflowx', 'visible');
+     // $('#viz iframe').css('overflowy', 'visible');
+     $('#viz iframe').width(size.width);
+     $('#viz iframe').height(size.height + 20);
+   }
 
-  /**
-   * Hook into the window resize event to resize when the user resizes the screen.
-   */
-  $(window).resize(resize);
+   /**
+    * Hook into the window resize event to resize when the user resizes the screen.
+    */
+   $(window).resize(resize);
